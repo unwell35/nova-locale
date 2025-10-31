5d6c6c3f6999454606b3420502a70ab894ca7230
unwell35 <mohamedsabbagh2001@gmail.com>
2025-10-31 04:12:43 +0200

    .

--- index.d.ts
+++ index.d.ts
@@ -2,21 +2,24 @@
 import type * as BotAR from "./ar/bot.json";
 import type * as BotDE from "./de/bot.json";
 import type * as BotEN from "./en/bot.json";
+import type * as BotFR from "./fr/bot.json";
 import type * as BotKU from "./ku/bot.json";
 import type * as BotTR from "./tr/bot.json";
 
 import type * as DashboardAR from "./ar/dashboard.json";
 import type * as DashboardDE from "./de/dashboard.json";
 import type * as DashboardEN from "./en/dashboard.json";
+import type * as DashboardFR from "./fr/dashboard.json";
 import type * as DashboardKU from "./ku/dashboard.json";
 import type * as DashboardTR from "./tr/dashboard.json";
 
-export type BotTranslation = typeof BotAR | typeof BotEN | typeof BotKU;
-export type DashboardTranslation = typeof DashboardAR | typeof DashboardEN | typeof DashboardKU;
+export type BotTranslation = typeof BotAR | typeof BotEN | typeof BotKU | typeof BotFR;
+export type DashboardTranslation = typeof DashboardAR | typeof DashboardEN | typeof DashboardKU | typeof DashboardFR;
 
 export interface BotLanguages {
   ar: typeof BotAR;
   en: typeof BotEN;
+  fr: typeof BotFR;
   ku: typeof BotKU;
   tr: typeof BotTR;
   de: typeof BotDE;
@@ -25,6 +28,7 @@ export interface BotLanguages {
 export interface DashboardLanguages {
   ar: typeof DashboardAR;
   en: typeof DashboardEN;
+  fr: typeof DashboardFR;
   ku: typeof DashboardKU;
   tr: typeof DashboardTR;
   de: typeof DashboardDE;
