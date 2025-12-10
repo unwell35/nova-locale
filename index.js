09d0ad14eb8c279f6ca5a26fb5c997cc0a55c433
unwell35 <mohamedsabbagh2001@gmail.com>
2025-12-11 01:51:19 +0200

    --

--- index.js
+++ index.js
@@ -6,12 +6,13 @@
  */
 
 /**
- * @type {Record<'ar' | 'en' | 'ku', BotTranslation>}
+ * @type {Record<'ar' | 'en' | 'ku' | 'fr', BotTranslation>}
  */
 const BOT_LANGUAGES = {
   ar: require("./ar/bot.json"),
   en: require("./en/bot.json"),
   ku: require("./ku/bot.json"),
+  fr: require("./fr/bot.json"),
 };
 
 /**
@@ -21,6 +22,7 @@ const DASHBOARD_LANGUAGES = {
   ar: require("./ar/dashboard.json"),
   en: require("./en/dashboard.json"),
   ku: require("./ku/dashboard.json"),
+  fr: require("./fr/dashboard.json"),
 };
 
 module.exports = {
