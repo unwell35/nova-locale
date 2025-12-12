ce7cc67201d9deedc61b7148900c808b72e656a8
unwell35 <mohamedsabbagh2001@gmail.com>
2025-12-12 19:28:15 +0200

    new strings

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,13 @@
 // Import the inferred JSON types
 import type * as BotAR from "./ar/bot.json";
-import type * as BotDE from "./de/bot.json";
 import type * as BotEN from "./en/bot.json";
 import type * as BotFR from "./fr/bot.json";
 import type * as BotKU from "./ku/bot.json";
-import type * as BotTR from "./tr/bot.json";
 
 import type * as DashboardAR from "./ar/dashboard.json";
-import type * as DashboardDE from "./de/dashboard.json";
 import type * as DashboardEN from "./en/dashboard.json";
 import type * as DashboardFR from "./fr/dashboard.json";
 import type * as DashboardKU from "./ku/dashboard.json";
-import type * as DashboardTR from "./tr/dashboard.json";
 
 export type BotTranslation = typeof BotAR | typeof BotEN | typeof BotKU | typeof BotFR;
 export type DashboardTranslation = typeof DashboardAR | typeof DashboardEN | typeof DashboardKU | typeof DashboardFR;
@@ -21,8 +17,6 @@ export interface BotLanguages {
   en: typeof BotEN;
   fr: typeof BotFR;
   ku: typeof BotKU;
-  tr: typeof BotTR;
-  de: typeof BotDE;
 }
 
 export interface DashboardLanguages {
@@ -30,8 +24,6 @@ export interface DashboardLanguages {
   en: typeof DashboardEN;
   fr: typeof DashboardFR;
   ku: typeof DashboardKU;
-  tr: typeof DashboardTR;
-  de: typeof DashboardDE;
 }
 
 export const BOT_LANGUAGES: BotLanguages;
