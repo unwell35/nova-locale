87132b0d742b8c20f3f65f201c48c2b967ac1f29
unwell35 <mohamedsabbagh2001@gmail.com>
2026-03-28 23:22:49 +0200

    Refactor code structure for improved readability and maintainability

--- index.d.ts
+++ index.d.ts
@@ -2,24 +2,24 @@
 import type * as BotAR from "./ar/bot.json";
 import type * as BotEN from "./en/bot.json";
 import type * as BotFR from "./fr/bot.json";
-import type * as BotKR from "./kr/bot.json";
+import type * as BotKO from "./ko/bot.json";
 import type * as BotKU from "./ku/bot.json";
 
 import type * as DashboardAR from "./ar/dashboard.json";
 import type * as DashboardEN from "./en/dashboard.json";
 import type * as DashboardFR from "./fr/dashboard.json";
-import type * as DashboardKR from "./kr/dashboard.json";
+import type * as DashboardKO from "./ko/dashboard.json";
 import type * as DashboardKU from "./ku/dashboard.json";
 
-export type BotTranslation = typeof BotAR | typeof BotEN | typeof BotKU | typeof BotFR | typeof BotKR;
-export type DashboardTranslation = typeof DashboardAR | typeof DashboardEN | typeof DashboardKU | typeof DashboardFR | typeof DashboardKR;
+export type BotTranslation = typeof BotAR | typeof BotEN | typeof BotKU | typeof BotFR | typeof BotKO;
+export type DashboardTranslation = typeof DashboardAR | typeof DashboardEN | typeof DashboardKU | typeof DashboardFR | typeof DashboardKO;
 
 export interface BotLanguages {
   ar: typeof BotAR;
   en: typeof BotEN;
   fr: typeof BotFR;
   ku: typeof BotKU;
-  kr: typeof BotKR;
+  kr: typeof BotKO;
 }
 
 export interface DashboardLanguages {
@@ -27,7 +27,7 @@ export interface DashboardLanguages {
   en: typeof DashboardEN;
   fr: typeof DashboardFR;
   ku: typeof DashboardKU;
-  kr: typeof DashboardKR;
+  kr: typeof DashboardKO;
 }
 
 export const BOT_LANGUAGES: BotLanguages;
