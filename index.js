26b5006314d1fe322c28a3e87c024075c9a44b05
unwell35 <mohamedsabbagh2001@gmail.com>
2026-03-28 23:33:25 +0200

    feat: add command examples for user and report actions in multiple languages

--- index.js
+++ index.js
@@ -6,23 +6,25 @@
  */
 
 /**
- * @type {Record<'ar' | 'en' | 'ku' | 'fr', BotTranslation>}
+ * @type {Record<'ar' | 'en' | 'ku' | 'fr' | 'ko', BotTranslation>}
  */
 const BOT_LANGUAGES = {
   ar: require("./ar/bot.json"),
   en: require("./en/bot.json"),
   ku: require("./ku/bot.json"),
   fr: require("./fr/bot.json"),
+  ko: require("./ko/bot.json"),
 };
 
 /**
- * @type {Record<'ar' | 'en' | 'ku', DashboardTranslation>}
+ * @type {Record<'ar' | 'en' | 'ku' | 'fr' | 'ko', DashboardTranslation>}
  */
 const DASHBOARD_LANGUAGES = {
   ar: require("./ar/dashboard.json"),
   en: require("./en/dashboard.json"),
   ku: require("./ku/dashboard.json"),
   fr: require("./fr/dashboard.json"),
+  ko: require("./ko/dashboard.json"),
 };
 
 module.exports = {
